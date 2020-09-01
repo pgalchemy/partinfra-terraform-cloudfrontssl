d140fb8a4cc91f74991e02532da3e219d6063cb1
Bryan Braun <bbraun7@gmail.com>
2020-09-01 17:38:54 -0400

    refactor: apply linting fixes and reorganize to 'Standard Module Structure'
    This is in preparation for publishing a private module to Terraform
    Cloud, as discussed here: https://github.com/pgalchemy/iot-platform/pull/2109#discussion_r482498620

--- main.tf
+++ main.tf
@@ -1,48 +1,26 @@
-variable "origin_domain_name" {}
-variable "origin_id" {}
-variable "alias" {}
-variable "acm_certificate_arn" {}
-variable "origin_path" {
-  default     = ""
-}
-variable "origin_access_identity" {
-  default     = ""
-}
-variable "distribution_enabled" {
-  default     = true
-}
-variable "comment" {
-  default     = ""
-}
-variable "default_root_object" {
-  default     = "index.html"
-}
-variable "compression" {
-  default     = false
-}
+## CLOUDFRONT CONFIG
 
-
-resource "aws_cloudfront_distribution" "ssl_distribution" {
+resource aws_cloudfront_distribution ssl_distribution {
   origin {
-    domain_name = "${var.origin_domain_name}"
-    origin_id   = "${var.origin_id}"
-    origin_path = "${var.origin_path}"
+    domain_name = var.origin_domain_name
+    origin_id   = var.origin_id
+    origin_path = var.origin_path
 
     s3_origin_config {
-      origin_access_identity = "${var.origin_access_identity}"
+      origin_access_identity = var.origin_access_identity
     }
   }
 
-  enabled             = "${var.distribution_enabled}"
-  comment             = "${var.comment}"
-  default_root_object = "${var.default_root_object}"
-  aliases = ["${var.alias}"]
+  enabled             = var.distribution_enabled
+  comment             = var.comment
+  default_root_object = var.default_root_object
+  aliases             = [var.alias]
 
   default_cache_behavior {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "${var.origin_id}"
-    compress         = "${var.compression}"
+    target_origin_id = var.origin_id
+    compress         = var.compression
 
     forwarded_values {
       query_string = false
@@ -59,33 +37,30 @@ resource "aws_cloudfront_distribution" "ssl_distribution" {
   }
 
   ordered_cache_behavior {
-      allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-      cached_methods   = ["GET", "HEAD", "OPTIONS"]
-      target_origin_id = "${var.origin_id}"
-      compress         = "${var.compression}"
-      path_pattern     = "/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = var.origin_id
+    compress         = var.compression
+    path_pattern     = "/*"
 
-      forwarded_values {
-        query_string = false
-        headers      = ["Origin"]
+    forwarded_values {
+      query_string = false
+      headers      = ["Origin"]
 
-        cookies {
-          forward = "none"
-        }
+      cookies {
+        forward = "none"
       }
+    }
 
-      viewer_protocol_policy = "redirect-to-https"
-      min_ttl                = 0
-      default_ttl            = 86400
-      max_ttl                = 31536000
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
 
-      lambda_function_association {
-          event_type = "${var.headers["enabled"] ? "viewer-response" : ""}"
-          // this currently does not work in Terraform
-          //lambda_arn = "${var.headers["enabled"] ? aws_lambda_function.headers.arn : ""}"
-          lambda_arn = "${aws_lambda_function.headers.arn}:${aws_lambda_function.headers.version}"
-
-        }
+    lambda_function_association {
+      event_type = var.headers["enabled"] ? "viewer-response" : ""
+      lambda_arn = aws_lambda_function.headers[0].qualified_arn
+    }
   }
 
   restrictions {
@@ -96,8 +71,8 @@ resource "aws_cloudfront_distribution" "ssl_distribution" {
   }
 
   viewer_certificate {
-    acm_certificate_arn = "${var.acm_certificate_arn}"
-    ssl_support_method = "sni-only"
+    acm_certificate_arn      = var.acm_certificate_arn
+    ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.2_2018"
   }
 
@@ -112,4 +87,79 @@ resource "aws_cloudfront_distribution" "ssl_distribution" {
     response_code      = 200
     response_page_path = "/index.html"
   }
+}
+
+
+# LAMBDA CONFIG
+
+data template_file function {
+  count    = var.headers["enabled"] ? 1 : 0
+  template = file("${path.module}/headers_function.js")
+
+  vars = {
+    hsts             = var.headers["hsts-enabled"]
+    x-content-type   = var.headers["x-content-type-enabled"]
+    x-frame-options  = var.headers["x-frame-options-enabled"]
+    x-xss-protection = var.headers["x-xss-protection-enabled"]
+  }
+}
+
+data archive_file headers_function {
+  count       = var.headers["enabled"] ? 1 : 0
+  type        = "zip"
+  output_path = "${path.module}/.zip/headers_function.zip"
+  source {
+    filename = "index.js"
+    content  = data.template_file.function[0].rendered
+  }
+}
+
+resource aws_iam_role headers_function {
+  count              = var.headers["enabled"] ? 1 : 0
+  name               = "${replace(var.alias, ".", "-")}-lambda"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": [
+          "lambda.amazonaws.com",
+          "edgelambda.amazonaws.com"
+        ]
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource aws_iam_role_policy_attachment headers_function_role_policy {
+  count      = var.headers["enabled"] ? 1 : 0
+  role       = aws_iam_role.headers_function[0].name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# Lambda@Edge requires that the function exist in the US East Region. See:
+# https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/lambda-requirements-limits.html#lambda-requirements-cloudfront-triggers
+provider "aws" {
+  alias  = "lambda_at_edge_region"
+  region = "us-east-1"
+}
+
+resource aws_lambda_function headers {
+  count            = var.headers["enabled"] ? 1 : 0
+  function_name    = "${replace(var.alias, ".", "-")}-headers"
+  filename         = data.archive_file.headers_function[0].output_path
+  source_code_hash = data.archive_file.headers_function[0].output_base64sha256
+  role             = aws_iam_role.headers_function[0].arn
+  runtime          = "nodejs12.x"
+  handler          = "index.handler"
+  memory_size      = 128
+  timeout          = 3
+  publish          = true
+  provider         = aws.lambda_at_edge_region
 }
